c5720402008291d897a4fd014275fc4e3cc9f64c
Veronika Kulieva <benikuben@yandex.ru>
2023-10-17 22:51:33 +0300

    Initial commit

---
+++ src/main/java/ru/neoflex/backendclientservice/BackEndClientServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.neoflex.backendclientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndClientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/neoflex/backendclientservice/BackEndClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.backendclientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
